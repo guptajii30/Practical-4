eeddac059a61db6279dfafb430b28c4154a47eca
guptajii30 <ganeshgupta10043@gmail.com>
2025-04-17 19:49:49 +0530

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,1 +1,7 @@
-
+cd D:\DevOps_practical_4
+  git init
+git add .
+git commit -m "Initial commit of Practical‑4 code"
+git branch -M main
+git remote add origin https://github.com/guptajii30/Practical-4.git
+git push -u origin main
